52dbbb969bd5845586e24c4a680fb6ae152019ba
Sajal Kumar Mishra <45098416+MSAJAL07@users.noreply.github.com>
2024-11-28 16:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (95.98%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -1,28 +1,41 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
- 
+
 class Solution {
-    public void it(List<Integer> res, TreeNode root){
-        if(root == null) return;
-        it(res, root.left);
-            res.add(root.val);
-
-        it(res,root.right);
-
+    public void it(List<Integer> res, TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode _root = root;
+        while (_root != null) {
+            stack.push(_root);
+            _root = _root.left;
+        }
+        TreeNode _root1;
+        while (!stack.isEmpty()) {
+            _root1 = stack.pop();
+            res.add(_root1.val);
+            if (_root1.right != null) {
+                _root1 = _root1.right;
+                while (_root1 != null) {
+                    stack.push(_root1);
+                    _root1 = _root1.left;
+                }
+            }
+        }
     }
+
     public List<Integer> inorderTraversal(TreeNode root) {
         Solution solution = new Solution();
         List<Integer> res = new ArrayList<Integer>();
