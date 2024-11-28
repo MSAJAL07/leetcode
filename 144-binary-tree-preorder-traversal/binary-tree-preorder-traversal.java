13c9c70c966cbe48744058d925d0b95b46aca1f4
Sajal Kumar Mishra <45098416+MSAJAL07@users.noreply.github.com>
2024-11-28 17:03:15 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (9.73%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -1,31 +1,50 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-      public void it(List<Integer> res, TreeNode root){
-        if(root == null) return;
-         res.add(root.val);
-        it(res, root.left);
-        it(res,root.right);
-       
+    // public void it(List<Integer> res, TreeNode root){
+    // if(root == null) return;
+    // res.add(root.val);
+    // it(res, root.left);
+    // it(res,root.right);
 
+    // }
+
+    // }
+    public void pr(List<Integer> res, TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode _root = root;
+        while (_root != null) {
+            stack.push(_root);
+            res.add(_root.val);
+            _root = _root.left;
+        }
+        while (!stack.isEmpty()) {
+            _root = stack.pop();
+            _root = _root.right;
+            while (_root != null) {
+                stack.push(_root);
+                res.add(_root.val);
+                _root = _root.left;
+            }
+        }
     }
     public List<Integer> preorderTraversal(TreeNode root) {
         Solution solution = new Solution();
         List<Integer> res = new ArrayList<Integer>();
-        solution.it(res, root);
+        solution.pr(res, root);
         return res;
     }
 }
