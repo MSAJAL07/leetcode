b843affd3551ea64c7d68510070ddbe169f34740
Sajal Kumar Mishra <45098416+MSAJAL07@users.noreply.github.com>
2025-02-17 01:39:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (14.67%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,17 @@
 class Solution {
-    int F(int n){
-        if(n == 0) return 0;
-        if( n == 1) return 1;
-        return F(n-1) + F(n-2);
+    int F(int n, unordered_map<int,int> &ump) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (ump.find(n) == ump.end())
+            ump[n] = F(n - 1, ump) + F(n - 2, ump);
+        return ump[n];
     }
+
 public:
     int fib(int n) {
-        return F(n);
+        unordered_map<int, int> ump;
+        return F(n, ump);
     }
 };
