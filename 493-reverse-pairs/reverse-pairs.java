895d50a8c5b724e0e822b31729c1e688c6dab5d7
Sajal Kumar Mishra <45098416+MSAJAL07@users.noreply.github.com>
2026-02-25 11:48:58 +0530

    Time: 38 ms (98.87%) | Memory: 60.5 MB (64.60%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.java
+++ 493-reverse-pairs/reverse-pairs.java
@@ -1,53 +1,36 @@
 class Solution {
-    private void merge(int[] arr, int left, int mid, int right){
-        int[] tmp = new int[right-left +1];
-        int i = left; int j = mid+1; int k =0;
-        while(i <= mid && j <= right){
-            if(arr[i] <= arr[j])
+    private int[] tmp; // single shared temp array
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int i = left, j = mid + 1, k = left;
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j])
                 tmp[k++] = arr[i++];
-            else if(arr[i]> arr[j])
+            else
                 tmp[k++] = arr[j++];
         }
-        while(i<=mid)
-            tmp[k++] = arr[i++];
-        while(j <= right) 
-            tmp[k++] = arr[j++];
-        for(i =left, k =0; i<=right; ++i, ++k){
-            arr[i] = tmp[k];
-        }
+        while (i <= mid) tmp[k++] = arr[i++];
+        while (j <= right) tmp[k++] = arr[j++];
+        System.arraycopy(tmp, left, arr, left, right - left + 1);
     }
 
     private int mergeSort(int[] nums, int left, int right) {
-        int result = 0;
-        if (left >= right)
-            return result;
+        if (left >= right) return 0;
+        int mid = left + (right - left) / 2; // avoids overflow vs (left+right)/2
+        int result = mergeSort(nums, left, mid)
+                   + mergeSort(nums, mid + 1, right);
 
-        int mid = (left + right) / 2;
-        result += mergeSort(nums, left, mid);
-        result += mergeSort(nums, mid + 1, right);
-        int j = mid+1;
-        for(int i = left; i<=mid; ++i){
-            while( j<=right && nums[i] > 2 * (long)nums[j]){
-                j++;
-            }
-            result += j -(mid + 1);
+        int j = mid + 1;
+        for (int i = left; i <= mid; ++i) {
+            while (j <= right && nums[i] > 2L * nums[j]) j++;
+            result += j - (mid + 1);
         }
         merge(nums, left, mid, right);
         return result;
-
     }
 
     public int reversePairs(int[] nums) {
-        // int result = 0;
-        // for (int i = 0; i < nums.length - 1; ++i) {
-        //     for (int j = i + 1; j < nums.length; ++j) {
-        //         if (nums[i] >  2L * nums[j]) {
-        //             result++;
-        //         }
-        //     }
-        // }
-        // return result;
+        tmp = new int[nums.length]; // allocate once
         return mergeSort(nums, 0, nums.length - 1);
-
     }
 }
