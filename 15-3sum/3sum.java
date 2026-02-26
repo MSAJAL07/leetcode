dcea426fda5ac3c680bbdc3897f2c2f195fd7656
Sajal Kumar Mishra <45098416+MSAJAL07@users.noreply.github.com>
2026-02-26 21:57:44 +0530

    Time: 35 ms (44.22%) | Memory: 59.1 MB (75.42%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,44 +1,27 @@
 class Solution {
-    // public boolean containsAnagram(List<List<Integer>> result, List<Integer> list) {
-    //     List<Integer> sortedTarget = new ArrayList<>(list);
-    //     Collections.sort(sortedTarget);
-
-    //     for (List<Integer> current : result) {
-
-    //         if (current.size() != list.size())
-    //             continue;
-
-    //         List<Integer> sortedCurrent = new ArrayList<>(current);
-    //         Collections.sort(sortedCurrent);
-
-    //         if (sortedCurrent.equals(sortedTarget)) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-    // }
-
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        Set<List<Integer>> seen = new HashSet<>();
-        for (int i = 0; i < nums.length - 2; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int target = -nums[i];
-            HashMap<Integer, Integer> map = new HashMap<>();
-            map.put(target - nums[i + 1], i + 1);
-            for (int j = i + 2; j < nums.length; ++j) {
-                if (map.containsKey(nums[j])) {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);
-                    list.add(nums[map.get(nums[j])]);
-                    list.add(nums[j]);
+        Arrays.sort(nums);
 
-                    List<Integer> key = list.stream().sorted().toList();
-                    if (seen.add(key)) {
-                        result.add(list);
-                    }
-                }
-                map.put(target - nums[j], j);
+        for (int i = 0; i < nums.length - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1])
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1])
+                        right--;
+                } else if (sum < 0)
+                    left++;
+                else
+                    right--;
             }
         }
         return result;
