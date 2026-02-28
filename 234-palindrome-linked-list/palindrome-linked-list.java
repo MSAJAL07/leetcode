f2010c0d94c8ee4cff25c727bef8fde18e6dac9b
Sajal Kumar Mishra <45098416+MSAJAL07@users.noreply.github.com>
2026-02-28 17:37:20 +0530

    Time: 3 ms (99.73%) | Memory: 96 MB (35.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,24 +9,31 @@
  * }
  */
 class Solution {
-    // private isP(ListNode head, ListNode current){
 
-    // }
+
     public boolean isPalindrome(ListNode head) {
-        Deque<ListNode> st = new ArrayDeque<>();
+        int count = 0;
         ListNode curr = head;
-        while(curr != null){
-            st.push(curr);
+        while(curr != null) {
+            count ++;
             curr = curr.next;
         }
         curr = head;
-        while(!st.isEmpty() && curr != null){
-            if(st.peek().val != curr.val) return false;
-            st.pop();
+        ListNode next = null;
+        for(int i = 0; i < count/2 ; ++i){
+            ListNode tmp = curr.next;
+            curr.next = next;
+            next = curr;
+            curr = tmp;
+        }
+        head.next = curr;
+        head = next;
+        if(count % 2 != 0) curr = curr.next;
+        for(int i = 0; i < count/2 ; ++i){
+            if(head.val != curr.val) return false;
+            head = head.next;
             curr = curr.next;
         }
         return true;
-
-        
     }
 }
